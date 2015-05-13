0d65b7a9a3464a812c914fc8592a75faa9d3b128
aarashtaheri <aarash.taheri@gmail.com>
2015-05-13 18:29:00 +0200

    First test commit

--- rock-paper-scissors/src/main/java/com/arash/rps/Console.java
+++ rock-paper-scissors/src/main/java/com/arash/rps/Console.java
@@ -6,28 +6,28 @@ import java.io.InputStreamReader;
 
 public class Console  {
 
-	public void write(String msg) {
-	}
-
-	public String getInput() {
-		java.io.Console c = System.console();
-		c.readLine();
-		
-		try {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-			String input = br.readLine();
-			return input;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
-	return null;
-	}
-
-	public void writeOutput(String msg) {
-		System.out.println(msg);		
-	}
+//	public void write(String msg) {
+//	}
+//
+//	public String getInput() {
+//		java.io.Console c = System.console();
+//		c.readLine();
+//		
+//		try {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//			String input = br.readLine();
+//			return input;
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}		
+//	return null;
+//	}
+//
+//	public void writeOutput(String msg) {
+//		System.out.println(msg);		
+//	}
 	
 	
 }
