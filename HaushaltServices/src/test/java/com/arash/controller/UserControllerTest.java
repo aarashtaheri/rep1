10b2ec53540a3f10452f97138667a9798676251a
Arash Taheri <aarash.taheri@gmail.com>
2015-11-10 13:04:34 +0100

    assert to see if service available

--- HaushaltServices/src/test/java/com/arash/controller/UserControllerTest.java
+++ HaushaltServices/src/test/java/com/arash/controller/UserControllerTest.java
@@ -7,7 +7,14 @@ import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.arash.Application;
 import com.arash.dao.UserDao;
@@ -15,6 +22,8 @@ import com.arash.model.ExpenseDTO;
 import com.arash.model.ExpenseEntity;
 import com.arash.model.UserEntity;
 import com.google.common.collect.Iterators;
+
+import net.wimpi.telnetd.io.terminal.ansi;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -28,9 +37,16 @@ public class UserControllerTest {
 	@Autowired
 	UserDao userDao;
 	
+	@Autowired
+	WebApplicationContext context;
+	
+	MockMvc mvc;
+	
 
 	@Before
 	public void setup() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
+		
 		UserEntity user1 = new UserEntity("user1", "user1@email.com");
 		userDao.save(user1);
 		System.out.println("created user id= " + user1.getId());
@@ -40,11 +56,13 @@ public class UserControllerTest {
 
 	//TODO use mock rest server
 	@Test
-	public void getOneUsers() {
-//		RestTemplate restTemplate = new RestTemplate();
-//
-//        UserEntity user= restTemplate.getForObject(server + "users/1", UserEntity.class);
-//        Assert.assertEquals("user1", user.getName());
+	public void getOneUsers() throws Exception {
+		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/users/1");
+		ResultActions result = mvc.perform(requestBuilder);
+		result.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		
+		
 	}
 	
 //	@Test
