47746ed7bf8f9d3df53d2f85be5e4f1702039f93
Arash Taheri <arash.taheri@akelius.de>
2015-11-23 15:36:45 +0100

    minor changes

--- HaushaltServices/src/main/java/com/arash/service/UserServiceImpl.java
+++ HaushaltServices/src/main/java/com/arash/service/UserServiceImpl.java
@@ -1,9 +1,7 @@
 package com.arash.service;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
@@ -26,139 +24,137 @@ public class UserServiceImpl implements IUserService {
 
 	@Autowired
 	UserDao userDao;
-	
+
 	@Autowired
 	ExpenseDao expenseDao;
-	
+
 	@Autowired
 	CalculationPeriodDao periodDao;
-	
+
 	@Override
 	public ExpenseEntity getExpenseByUserIdAndCalculationPeriod(int userId, int calculationPeriod) {
 
-		
-//		@SuppressWarnings("unused")
-//		Iterable<ExpenseEntity> espensesForPeriod = expenseDao.findByCalculationPeriod(new CalculationPeriodEntity(11,2105));
+		// @SuppressWarnings("unused")
+		// Iterable<ExpenseEntity> espensesForPeriod =
+		// expenseDao.findByCalculationPeriod(new
+		// CalculationPeriodEntity(11,2105));
 
-		
-		List<ExpenseEntity> expenses = (List<ExpenseEntity>) userDao.findOne(userId).getExpenses();
-			
-		ExpenseEntity exp = expenses.stream()
-				 .filter(p -> p.getCalculationPeriod().getId() == calculationPeriod)
-				 .findFirst().get();
-		 
+		final List<ExpenseEntity> expenses = (List<ExpenseEntity>) userDao.findOne(userId).getExpenses();
+
+		final ExpenseEntity exp = expenses.stream().filter(p -> p.getCalculationPeriod().getId() == calculationPeriod)
+				.findFirst().get();
+
 		return exp;
 	}
-	
+
+	@Override
 	public ExpenseDTO getExpenseByUserIdAndCalculationPeriod2(int userId, int calculationPeriod) {
-		
-		List<ExpenseEntity> expenses = (List<ExpenseEntity>) userDao.findOne(userId).getExpenses();
-		
+
+		final List<ExpenseEntity> expenses = (List<ExpenseEntity>) userDao.findOne(userId).getExpenses();
+
 		// find element in list using lambda
-		ExpenseEntity exp = expenses.stream()
-				 .filter(p -> p.getCalculationPeriod().getId() == calculationPeriod)
-				 .findFirst().get();
-		
-		ExpenseDTO expenseDTO = new ExpenseDTO();
-		
+		final ExpenseEntity exp = expenses.stream().filter(p -> p.getCalculationPeriod().getId() == calculationPeriod)
+				.findFirst().get();
+
+		final ExpenseDTO expenseDTO = new ExpenseDTO();
+
 		expenseDTO.setUserId(userId);
-		
+
 		expenseDTO.setMyBalance(exp.getBalance());
 		expenseDTO.setSumMyExpenses(exp.getSumExpense());
-		
 
-		List<ExpenseEntity> expensesAll = Lists.newArrayList( expenseDao.findAll());
-		
-		//filter elements in a list using lambda
-		List<ExpenseEntity> expensesForPeriod = expensesAll.stream().
-		filter(e -> e.getCalculationPeriod().getId() == calculationPeriod).collect(Collectors.toList());
-		
-		//sum using lambda
-		double sumExpsensesInPeriod = expensesAll.stream().
-				filter(e -> e.getCalculationPeriod().getId() == calculationPeriod)
+		final List<ExpenseEntity> expensesAll = Lists.newArrayList(expenseDao.findAll());
+
+		// filter elements in a list using lambda
+		final List<ExpenseEntity> expensesForPeriod = expensesAll.stream()
+				.filter(e -> e.getCalculationPeriod().getId() == calculationPeriod).collect(Collectors.toList());
+
+		// sum using lambda
+		final double sumExpsensesInPeriod = expensesAll.stream()
+				.filter(e -> e.getCalculationPeriod().getId() == calculationPeriod)
 				.collect(Collectors.summarizingDouble(ExpenseEntity::getSumExpense)).getSum();
-		
+
 		expenseDTO.setSumEspensesOfAllUsers((float) sumExpsensesInPeriod);
-		expenseDTO.setEachOneShouldPay((float) (sumExpsensesInPeriod/3));
-		
+		expenseDTO.setEachOneShouldPay((float) (sumExpsensesInPeriod / 3));
+
 		System.out.println(sumExpsensesInPeriod);
-		 
+
 		return expenseDTO;
-	}
-
-	@Transactional
-	@Override
-	public UserEntity updateExpense(int userId, int calculationPeriod, float newValue) {
-		// 1 . update the value in the database
-		UserEntity user = userDao.findOne(userId);
-		
-		ExpenseEntity expense = getExpenseByUserIdAndCalculationPeriod(userId, calculationPeriod);
-		expense.setSumExpense(newValue);
-		expenseDao.save(expense);
-				
-		// 2. update the balance of all users according to the new expense
-		float sumAllExpenses = 0;
-		// TODO use lambda
-		Collection<UserEntity> users = (Collection<UserEntity>) userDao.findAll();
-		for (UserEntity userEntity : users) {
-			sumAllExpenses +=  getExpenseByUserIdAndCalculationPeriod(userEntity.getId(), calculationPeriod).getSumExpense();
-		}
-
-		int numberOfUsers =  Iterators.size(users.iterator());
-		
-		for (UserEntity userEntity : users) {
-			expense = getExpenseByUserIdAndCalculationPeriod(userEntity.getId(), calculationPeriod);
-			double balance = expense.getSumExpense() - (sumAllExpenses/numberOfUsers);
-			expense.setBalance(balance);
-			expenseDao.save(expense);
-		}
-
-		return user;
 	}
 
 	@Override
 	public void initDB() {
-		//only execute if the database is empty
+		// only execute if the database is empty
 		if (userDao.findAll().iterator().hasNext()) {
 			return;
 		}
-		
-		UserEntity arash = new UserEntity("arash", "");
+
+		final UserEntity arash = new UserEntity("arash", "");
 		userDao.save(arash);
-		
-		UserEntity omid = new UserEntity("omid", "");
+
+		final UserEntity omid = new UserEntity("omid", "");
 		userDao.save(omid);
-		
-		UserEntity afsane = new UserEntity("afsane", "");
+
+		final UserEntity afsane = new UserEntity("afsane", "");
 		userDao.save(afsane);
-		
-		CalculationPeriodEntity c1 = new CalculationPeriodEntity(11,2015);
-		CalculationPeriodEntity c2 = new CalculationPeriodEntity(12,2015);
-		CalculationPeriodEntity c3 = new CalculationPeriodEntity(1,2016);
-		CalculationPeriodEntity c4 = new CalculationPeriodEntity(2,2016);
-		CalculationPeriodEntity c5 = new CalculationPeriodEntity(3,2016);
-		CalculationPeriodEntity c6 = new CalculationPeriodEntity(4,2016);
-		
+
+		final CalculationPeriodEntity c1 = new CalculationPeriodEntity(11, 2015);
+		final CalculationPeriodEntity c2 = new CalculationPeriodEntity(12, 2015);
+		final CalculationPeriodEntity c3 = new CalculationPeriodEntity(1, 2016);
+		final CalculationPeriodEntity c4 = new CalculationPeriodEntity(2, 2016);
+		final CalculationPeriodEntity c5 = new CalculationPeriodEntity(3, 2016);
+		final CalculationPeriodEntity c6 = new CalculationPeriodEntity(4, 2016);
+
 		periodDao.save(c1);
 		periodDao.save(c2);
 		periodDao.save(c3);
 		periodDao.save(c4);
 		periodDao.save(c5);
 		periodDao.save(c6);
-		
-		ExpenseEntity exp1_arash = new ExpenseEntity(arash, c1);
-		ExpenseEntity exp1_omid = new ExpenseEntity(omid, c1);
-		ExpenseEntity exp1_afsane = new ExpenseEntity(afsane, c1);
-		ExpenseEntity exp2_arash = new ExpenseEntity(arash, c2);
-		ExpenseEntity exp2_omid = new ExpenseEntity(omid, c2);
-		ExpenseEntity exp2_afsane = new ExpenseEntity(afsane, c2);
+
+		final ExpenseEntity exp1_arash = new ExpenseEntity(arash, c1);
+		final ExpenseEntity exp1_omid = new ExpenseEntity(omid, c1);
+		final ExpenseEntity exp1_afsane = new ExpenseEntity(afsane, c1);
+		final ExpenseEntity exp2_arash = new ExpenseEntity(arash, c2);
+		final ExpenseEntity exp2_omid = new ExpenseEntity(omid, c2);
+		final ExpenseEntity exp2_afsane = new ExpenseEntity(afsane, c2);
 		expenseDao.save(exp1_arash);
 		expenseDao.save(exp1_omid);
 		expenseDao.save(exp1_afsane);
 		expenseDao.save(exp2_arash);
 		expenseDao.save(exp2_omid);
 		expenseDao.save(exp2_afsane);
-		
+
+	}
+
+	@Transactional
+	@Override
+	public UserEntity updateExpense(int userId, int calculationPeriod, float newValue) {
+		// 1 . update the value in the database
+		final UserEntity user = userDao.findOne(userId);
+
+		ExpenseEntity expense = getExpenseByUserIdAndCalculationPeriod(userId, calculationPeriod);
+		expense.setSumExpense(newValue);
+		expenseDao.save(expense);
+
+		// 2. update the balance of all users according to the new expense
+		float sumAllExpenses = 0;
+		final Collection<UserEntity> users = (Collection<UserEntity>) userDao.findAll();
+		for (final UserEntity userEntity : users) {
+			sumAllExpenses += getExpenseByUserIdAndCalculationPeriod(userEntity.getId(), calculationPeriod)
+					.getSumExpense();
+		}
+
+		final int numberOfUsers = Iterators.size(users.iterator());
+
+		for (final UserEntity userEntity : users) {
+			expense = getExpenseByUserIdAndCalculationPeriod(userEntity.getId(), calculationPeriod);
+			final double balance = expense.getSumExpense() - (sumAllExpenses / numberOfUsers);
+			expense.setBalance(balance);
+			expenseDao.save(expense);
+		}
+
+		return user;
 	}
 
 }
--- HaushaltServices/src/test/java/com/arash/controller/UserControllerTest.java
+++ HaushaltServices/src/test/java/com/arash/controller/UserControllerTest.java
@@ -1,5 +1,7 @@
 package com.arash.controller;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,113 +15,110 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.arash.Application;
 import com.arash.ApplicationTest;
 import com.arash.dao.CalculationPeriodDao;
 import com.arash.dao.ExpenseDao;
 import com.arash.dao.UserDao;
 import com.arash.model.CalculationPeriodEntity;
-import com.arash.model.ExpenseDTO;
 import com.arash.model.ExpenseEntity;
 import com.arash.model.UserEntity;
 import com.arash.service.IUserService;
 
-import org.junit.Assert;
-import org.junit.Before;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes=ApplicationTest.class)
+@SpringApplicationConfiguration(classes = ApplicationTest.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:0")
 public class UserControllerTest {
-	
-	@Autowired UserDao userDao;
-	@Autowired ExpenseDao expenseDao;
-	@Autowired CalculationPeriodDao periodDao;
-	
-	
-	@Autowired WebApplicationContext context;
-	
-	@Autowired IUserService userService;
-	
+
+	@Autowired
+	UserDao userDao;
+	@Autowired
+	ExpenseDao expenseDao;
+	@Autowired
+	CalculationPeriodDao periodDao;
+
+	@Autowired
+	WebApplicationContext context;
+
+	@Autowired
+	IUserService userService;
+
 	MockMvc mvc;
-	
+
 	UserEntity user1, user2;
 	ExpenseEntity expense1, expense2;
 	CalculationPeriodEntity calculationPeriod;
 
+	@Test
+	public void getAllUsers_Test() throws Exception {
+		final RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/users/all");
+		final ResultActions result = mvc.perform(requestBuilder);
+		// if service available
+		result.andExpect(MockMvcResultMatchers.status().isOk());
+
+		// see the content of response
+		final String content = mvc.perform(requestBuilder).andReturn().getResponse().getContentAsString();
+		System.out.println(content);
+
+	}
+
+	@Test
+	public void getOneUser_Test() throws Exception {
+
+		final RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/users/1");
+		final ResultActions result = mvc.perform(requestBuilder);
+		// if service available
+		result.andExpect(MockMvcResultMatchers.status().isOk());
+
+		// see the content of response
+		final String content = mvc.perform(requestBuilder).andReturn().getResponse().getContentAsString();
+		final String expected = "{\"id\":1,\"email\":\"user1\",\"name\":\"user1@email.com\"}";
+		Assert.assertEquals(expected, content);
+
+		final RequestBuilder requestBuilder2 = MockMvcRequestBuilders.get("/users/2");
+		;
+		final String content2 = mvc.perform(requestBuilder2).andReturn().getResponse().getContentAsString();
+		final String expected2 = "{\"id\":2,\"email\":\"user2\",\"name\":\"user2@email.com\"}";
+		Assert.assertEquals(expected2, content2);
+
+		System.out.println(content);
+		System.out.println(content2);
+
+	}
+
 	@Before
 	public void setup() {
-		this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
-		
+		mvc = MockMvcBuilders.webAppContextSetup(context).build();
+
 		user1 = new UserEntity("user1", "user1@email.com");
 		userDao.save(user1);
 		user2 = new UserEntity("user2", "user2@email.com");
 		userDao.save(user2);
-		
+
 		calculationPeriod = new CalculationPeriodEntity(10, 2015);
 		periodDao.save(calculationPeriod);
-		
+
 		expense1 = new ExpenseEntity(user1, calculationPeriod);
 		expense2 = new ExpenseEntity(user2, calculationPeriod);
 		expenseDao.save(expense1);
 		expenseDao.save(expense2);
-						
-     }
-	
+
+	}
+
 	@Test
 	public void testInsertedData() {
 		Assert.assertEquals(user1, expense1.getUser());
 		Assert.assertEquals(user2, expense2.getUser());
-		
-	}
-	
-	String server = "http://localhost:0/";
 
-	@Test
-	public void getOneUser_Test() throws Exception {
-		
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/users/1");
-		ResultActions result = mvc.perform(requestBuilder);
-		//if service available
-		result.andExpect(MockMvcResultMatchers.status().isOk());
-		
-		
-		//see the content of response
-		String content = mvc.perform(requestBuilder).andReturn().getResponse().getContentAsString();
-		String expected = "{\"id\":1,\"email\":\"user1\",\"name\":\"user1@email.com\"}";
-		Assert.assertEquals(expected, content);
-		
-		RequestBuilder requestBuilder2 = MockMvcRequestBuilders.get("/users/2");;
-		String content2 = mvc.perform(requestBuilder2).andReturn().getResponse().getContentAsString();
-		String expected2 = "{\"id\":2,\"email\":\"user2\",\"name\":\"user2@email.com\"}";
-		Assert.assertEquals(expected2, content2);
-		
-		System.out.println(content);
-		System.out.println(content2);
-		
-	}
-	
-	@Test
-	public void getAllUsers_Test() throws Exception {
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/users/all");
-		ResultActions result = mvc.perform(requestBuilder);
-		//if service available
-		result.andExpect(MockMvcResultMatchers.status().isOk());
-		
-		//see the content of response
-		String content = mvc.perform(requestBuilder).andReturn().getResponse().getContentAsString();
-		System.out.println(content);		
-		
 	}
 
-//	@Test
-//	public void getExpense() {
-//        RestTemplate restTemplate = new RestTemplate();
-//        ExpenseDTO expense = restTemplate.getForObject(server + "users/1/getExpense?periodId=1", ExpenseDTO.class);
-//        Assert.assertNotNull(expense);
-//	}
+	// @Test
+	// public void getExpense() {
+	// RestTemplate restTemplate = new RestTemplate();
+	// ExpenseDTO expense = restTemplate.getForObject(server +
+	// "users/1/getExpense?periodId=1", ExpenseDTO.class);
+	// Assert.assertNotNull(expense);
+	// }
 }
